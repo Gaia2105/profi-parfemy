4cacbb0591f13517495b44f1f06af1be90f5b3f1
gaiane <sarkisian.gaiane@gmail.com>
2025-06-05 19:26:01 +0200

    new

--- profi-parfemy-test/cypress.config.ts
+++ profi-parfemy-test/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'https://www.profi-parfemy.cz',
+    projectId: "wtpxy4",
     setupNodeEvents(on, config) {
       // events, если нужны
     }
