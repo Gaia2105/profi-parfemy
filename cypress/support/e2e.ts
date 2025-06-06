aa23992194e6bc26c4638a8dc5b43b4fe06f82f4
gaiane <sarkisian.gaiane@gmail.com>
2025-06-06 21:43:25 +0200

    new

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,3 @@
-// cypress/support/e2e.ts
-
-// Импорт кастомных команд (если есть)
 import './commands'
 
-// Можно добавить глобальные хуки:
-// beforeEach(() => {
-//   cy.log('This runs before every test')
-// })
+ 
